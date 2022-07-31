5cd7f8862d4af176e4812b96bd8bb6d4c0c6a861
Rostislav Zapolskiy <rostislav.zapolskiy@gmail.com>
2022-07-31 06:51:06 +0400

    Added RemoteFeedLoader basic tests

---
+++ feed-appTests/RemoteFeedLoaderTests.swift
@@ -0,0 +1,42 @@
+//
+//  RemoteFeedLoaderTests.swift
+//  feed-appTests
+//
+//  Created by Rostislav Zapolsky on 30.07.22.
+//
+
+import XCTest
+
+class RemoteFeedLoader {
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string: "http://a-url.com")
+    }
+}
+
+class HTTPClient {
+    static let shared = HTTPClient()
+    
+    private init() {}
+    
+    var requestedURL: URL?
+}
+
+class RemoteFeedLoaderTests: XCTestCase {
+
+    func test_init_doesNotRequestDataFromURL() {
+        let client = HTTPClient.shared
+        _ = RemoteFeedLoader()
+                
+        XCTAssertNil(client.requestedURL)
+    }
+
+    func test_load_requestDataFromURL() {
+        let client = HTTPClient.shared
+        let sut = RemoteFeedLoader()
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
+    }
+    
+}
