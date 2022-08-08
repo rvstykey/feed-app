7d937b9a86553d34b6cbdeae7468187e652a1e08
Rostislav Zapolskiy <rostislav.zapolskiy@gmail.com>
2022-08-08 14:15:04 +0300

    Moved decoding logic into separate class

--- feed-app/Feed API/RemoteFeedLoader.swift
+++ feed-app/Feed API/RemoteFeedLoader.swift
@@ -39,18 +39,40 @@ public final class RemoteFeedLoader {
         client.get(from: url) { result in
             switch result {
             case let .success(data, response):
-                if response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) {
-                    completion(.success(root.items))
-                } else {
+                do {
+                    let items = try FeedItemMapper.map(data, response)
+                    completion(.success(items))
+                } catch {
                     completion(.failure(.invalidData))
                 }
             case .failure:
-                completion(.failure(.connectivity ))
+                completion(.failure(.connectivity))
             }
         }
     }
 }
- 
-private struct Root: Decodable {
-    let items: [FeedItem]
+
+private class FeedItemMapper {
+    private struct Root: Decodable {
+        let items: [Item]
+    }
+    
+    private struct Item: Decodable {
+        public let id: UUID
+        public let description: String?
+        public let location: String?
+        public let image: URL
+        
+        var item: FeedItem {
+            return FeedItem(id: id, description: description, location: location, imageURL: image)
+        }
+    }
+    
+    static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
+        guard response.statusCode == 200 else {
+            throw RemoteFeedLoader.Error.invalidData
+        }
+        let root = try JSONDecoder().decode(Root.self, from: data)
+        return root.items.map { $0.item }
+    }
 }
