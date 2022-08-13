5ccba62f438708dd1d8759bdb894984246e12689
Rostislav Zapolskiy <rostislav.zapolskiy@gmail.com>
2022-08-14 00:27:15 +0300

    Updated test account expected values in EndToEndTests

--- feed-appEndToEndTests/feed_appEndToEndTests.swift
+++ feed-appEndToEndTests/feed_appEndToEndTests.swift
@@ -42,7 +42,7 @@ class feed_appEndToEndTests: XCTestCase {
             exp.fulfill()
         }
         
-        wait(for: [exp], timeout: 5.0)
+        wait(for: [exp], timeout: 10.0)
         return receivedResult
     }
     
@@ -56,20 +56,41 @@ class feed_appEndToEndTests: XCTestCase {
     
     private func id(at index: Int) -> UUID {
         return UUID(uuidString: [
-        
+            "73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6",
+            "BA298A85-6275-48D3-8315-9C8F7C1CD109",
+            "5A0D45B3-8E26-4385-8C5D-213E160A5E3C",
+            "FF0ECFE2-2879-403F-8DBE-A83B4010B340",
+            "DC97EF5E-2CC9-4905-A8AD-3C351C311001",
+            "557D87F1-25D3-4D77-82E9-364B2ED9CB30",
+            "A83284EF-C2DF-415D-AB73-2A9B8B04950B",
+            "F79BD7F8-063F-46E2-8147-A67635C3BB01"
         ][index])!
     }
     
     private func description(at index: Int) -> String? {
         return [
-            
-        ][index]!
+            "Description 1",
+            nil,
+            "Description 3",
+            nil,
+            "Description 5",
+            "Description 6",
+            "Description 7",
+            "Description 8"
+        ][index]
     }
     
     private func location(at index: Int) -> String? {
         return [
-            
-        ][index]!
+            "Location 1",
+            "Location 2",
+            nil,
+            nil,
+            "Location 5",
+            "Location 6",
+            "Location 7",
+            "Location 8"
+        ][index]
     }
     
     private func imageURL(at index: Int) -> URL {
