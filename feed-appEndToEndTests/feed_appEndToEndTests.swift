7ee8b99dc2e313b610139333468a375cb0f9023a
Rostislav Zapolskiy <rostislav.zapolskiy@gmail.com>
2022-08-13 22:52:50 +0300

    Added end-to-end Feed API tests

---
+++ feed-appEndToEndTests/feed_appEndToEndTests.swift
@@ -0,0 +1,73 @@
+//
+//  feed_appEndToEndTests.swift
+//  feed-appEndToEndTests
+//
+//  Created by Rostislav Zapolsky on 13.08.22.
+//
+
+import XCTest
+import feed_app
+
+class feed_appEndToEndTests: XCTestCase {
+
+    func test_endToEndServerGetFeedResult_matchesFixedTestAccountData() {
+        let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
+        let client = URLSessionHTTPClient()
+        let loader = RemoteFeedLoader(url: testServerURL, client: client)
+        
+        let exp = expectation(description: "Wait for load completion")
+        
+        var receivedResult: LoadFeedResult?
+        loader.load { result in
+            receivedResult = result
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 5.0)
+        
+        switch receivedResult {
+        case let .success(items):
+            XCTAssertEqual(items.count, 8, "Expected 8 items in test account feed")
+            
+            items.enumerated().forEach { (index, item) in
+                XCTAssertEqual(item, expectedItem(at: index), "Unexpected item values at \(index)")
+            }
+        case let .failure(error):
+            XCTFail("Expected successful feed result, got \(error) instead")
+        default:
+            XCTFail("Expected successful feed result, got no result instead")
+        }
+    }
+    
+    // MARK: - Helpers
+    
+    private func expectedItem(at index: Int) -> FeedItem {
+        return FeedItem(
+            id: id(at: index),
+            description: description(at: index),
+            location: location(at: index),
+            imageURL: imageURL(at: index))
+    }
+    
+    private func id(at index: Int) -> UUID {
+        return UUID(uuidString: [
+        
+        ][index])!
+    }
+    
+    private func description(at index: Int) -> String? {
+        return [
+            
+        ][index]!
+    }
+    
+    private func location(at index: Int) -> String? {
+        return [
+            
+        ][index]!
+    }
+    
+    private func imageURL(at index: Int) -> URL {
+        return URL(string: "https://url-\(index+1).com")!
+    }
+}
