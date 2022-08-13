0b6b0e655fc457435b58fb97a3300da6fd65184d
Rostislav Zapolskiy <rostislav.zapolskiy@gmail.com>
2022-08-13 11:07:13 +0300

    URLSessionHTTPClientTests: Added anyURL helper method

--- feed-appTests/Feed API/URLSessionHTTPClientTests.swift
+++ feed-appTests/Feed API/URLSessionHTTPClientTests.swift
@@ -39,7 +39,7 @@ class URLSessionHTTPClientTests: XCTestCase {
     }
     
     func test_getFromURL_performsGETRequestWithURL() {
-        let url = URL(string: "http://a-url.com")!
+        let url = anyURL()
         let exp = expectation(description: "Wait for request")
         
         URLProtocolStub.observeRequests { request in
@@ -54,13 +54,12 @@ class URLSessionHTTPClientTests: XCTestCase {
     }
     
     func test_getFromURL_failsOnRequestError() {
-        let url = URL(string: "http://a-url.com")!
         let error = NSError(domain: "any error", code: 1)
         URLProtocolStub.stub(data: nil, response: nil, error: error)
         
         let exp = expectation(description: "Wait for completion")
         
-        makeSUT().get(url: url) { result in
+        makeSUT().get(url: anyURL()) { result in
             switch result {
             case let .failure(receivedError as NSError):
                 XCTAssertEqual(receivedError.domain, error.domain)
@@ -81,6 +80,11 @@ class URLSessionHTTPClientTests: XCTestCase {
         let sut = URLSessionHTTPClient()
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
+    }
+    
+    private func anyURL() -> URL {
+        let url = URL(string: "http://any-url.com")!
+        return url
     }
     
     private class URLProtocolStub: URLProtocol {
