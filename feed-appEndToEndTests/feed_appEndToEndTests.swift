922319405fd88a28ea694c6e8a5755f667429111
Rostislav Zapolskiy <rostislav.zapolskiy@gmail.com>
2022-08-19 18:30:41 +0300

    Disable caching for endToEnd tests request

--- feed-appEndToEndTests/feed_appEndToEndTests.swift
+++ feed-appEndToEndTests/feed_appEndToEndTests.swift
@@ -29,7 +29,7 @@ class feed_appEndToEndTests: XCTestCase {
     
     private func getFeedResult(file: StaticString = #filePath, line: UInt = #line) -> LoadFeedResult? {
         let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
-        let client = URLSessionHTTPClient()
+        let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
         let loader = RemoteFeedLoader(url: testServerURL, client: client)
         trackForMemoryLeaks(client, file: file, line: line)
         trackForMemoryLeaks(loader, file: file, line: line)
