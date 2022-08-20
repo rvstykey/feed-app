ae7e6ea1a6461522f854f1e5a2f250d7d1b555c6
Rostislav Zapolskiy <rostislav.zapolskiy@gmail.com>
2022-08-20 18:08:53 +0300

    Does not insert cache on deletion error

--- feed-appTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ feed-appTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -23,9 +23,14 @@ class LocalFeedLoader {
 
 class FeedStore {
     var deleteCacheFeedCallCount = 0
+    var insertCallCount = 0
     
     func deleteCachedFeed() {
         deleteCacheFeedCallCount += 1
+    }
+    
+    func completeDeletion(with error: Error, at index: Int = 0) {
+         
     }
 }
 
@@ -46,6 +51,17 @@ class CacheFeedUseCaseTests: XCTestCase {
         XCTAssertEqual(store.deleteCacheFeedCallCount, 1)
     }
     
+    func test_save_doesNotRequestCacheInsertionOnDeletionError() {
+        let (sut, store) = makeSut()
+        let items = [uniqueItem(), uniqueItem()]
+        
+        let error = anyNSError()
+        sut.save(items)
+        store.completeDeletion(with: error)
+        
+        XCTAssertEqual(store.insertCallCount, 0)
+    }
+    
     // MARK: Helpers
     
     private func makeSut(file: StaticString = #filePath, line: UInt = #line) -> (LocalFeedLoader, FeedStore) {
@@ -62,5 +78,9 @@ class CacheFeedUseCaseTests: XCTestCase {
     
     private func anyURL() -> URL {
         return URL(string: "http://any-url.com")!
+    }
+    
+    private func anyNSError() -> NSError {
+        return NSError(domain: "any error", code: 0)
     }
 }
