b7e7c65b9eb8bafaf9dac54867fe5d90df1931c3
Rostislav Zapolskiy <rostislav.zapolskiy@gmail.com>
2022-08-20 17:47:33 +0300

    Added does not delete cache upon creation test

---
+++ feed-appTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -0,0 +1,28 @@
+//
+//  CacheFeedUseCaseTests.swift
+//  feed-appTests
+//
+//  Created by Rostislav Zapolsky on 20.08.22.
+//
+
+import XCTest
+
+class LocalFeedLoader {
+    init(store: FeedStore) {
+        
+    }
+}
+
+class FeedStore {
+    var deleteCacheFeedCallCount = 0
+}
+
+class CacheFeedUseCaseTests: XCTestCase {
+    
+    func test_init_doesNotDeleteCacheUponCreation() {
+        let store = FeedStore()
+        _ = LocalFeedLoader(store: store)
+        
+        XCTAssertEqual(store.deleteCacheFeedCallCount, 0)
+    }
+}
