d050d5da46c40bbaf0001677ec0d469a4696589e
Rostislav Zapolskiy <rostislav.zapolskiy@gmail.com>
2022-08-26 16:00:06 +0400

    Extacted LocalFeedLoader to production module

---
+++ feed-app/Feed Cache/FeedStore.swift
@@ -0,0 +1,16 @@
+//
+//  FeedStore.swift
+//  feed-app
+//
+//  Created by Rostislav Zapolsky on 26.08.22.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
