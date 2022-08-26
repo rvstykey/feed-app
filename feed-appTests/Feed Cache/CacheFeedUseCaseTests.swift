cb9d7a431ff3117bf54024c1ab73441f3e7b4379
Rostislav Zapolskiy <rostislav.zapolskiy@gmail.com>
2022-08-26 15:37:05 +0400

    Guaranteed LocalFeedLoader doesn't deliver deletion error after instance deallocation

--- feed-appTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ feed-appTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -19,7 +19,8 @@ class LocalFeedLoader {
     }
     
     func save(_ items: [FeedItem], completion: @escaping (Error?) -> Void) {
-        store.deleteCachedFeed { [unowned self] error in
+        store.deleteCachedFeed { [weak self] error in
+            guard let self = self else { return }
             if error == nil {
                 self.store.insert(items, timestamp: self.currentDate(), completion: completion)
             } else {
@@ -102,6 +103,20 @@ class CacheFeedUseCaseTests: XCTestCase {
             store.completeDeletionSuccessfully()
             store.completeInsertionSuccessfully()
         }
+    }
+    
+    func test_save_doesNotDeliverDeletionErrorAfterSUTInstanceHasBeenDeallocatd() {
+        let store = FeedStoreSpy()
+        var sut: LocalFeedLoader? = LocalFeedLoader(store: store, currentDate: Date.init)
+        
+        var receivedResults = [Error?]()
+        sut?.save([uniqueItem()]) { error in
+            receivedResults.append(error)
+        }
+        
+        sut = nil
+        store.completeDeletion(with: anyNSError())
+        XCTAssertTrue(receivedResults.isEmpty)
     }
     
     // MARK: Helpers
