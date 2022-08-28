e6140cd789d357455669e0571baea697bd434308
Rostislav Zapolskiy <rostislav.zapolskiy@gmail.com>
2022-08-29 01:03:31 +0400

    Added RemoteFeedItem to decouple items mapper from FeedItem

--- feed-app/Feed API/RemoteFeedLoader.swift
+++ feed-app/Feed API/RemoteFeedLoader.swift
@@ -28,10 +28,27 @@ public final class RemoteFeedLoader: FeedLoader {
             guard self != nil else { return }
             switch result {
             case let .success(data, response):
-                completion(FeedItemMapper.map(data, response: response))
+                completion(RemoteFeedLoader.map(data, from: response))
             case .failure:
                 completion(.failure(Error.connectivity))
             }
+        }
+    }
+    
+    private static func map(_ data: Data, from response: HTTPURLResponse) -> Result {
+        do {
+            let items = try FeedItemMapper.map(data, response: response)
+            return .success(items.toModels())
+        } catch {
+            return .failure(error)
+        }
+    }
+}
+
+private extension Array where Element == RemoteFeedItem {
+    func toModels() -> [FeedItem] {
+        return map {
+            FeedItem(id: $0.id, description: $0.description, location: $0.location, imageURL: $0.image)
         }
     }
 }
