3bbc349b3cd4a0310252bbbe429126c1be039132
Rostislav Zapolskiy <rostislav.zapolskiy@gmail.com>
2022-08-29 12:33:40 +0400

    Does not delete cache when cache is already empty

--- feed-appTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ feed-appTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -82,6 +82,15 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
         XCTAssertEqual(store.receivedMessages, [.retrieve, .deleteCachedFeed])
     }
     
+    func test_load_doesNotdeletesCacheOnRetrievalError() {
+        let (sut, store) = makeSut()
+
+        sut.load { _ in }
+        store.completeRetrievalWithEmptyCache()
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve])
+    }
+    
     // MARK: Helpers
     
     private func makeSut(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (LocalFeedLoader, FeedStoreSpy) {
