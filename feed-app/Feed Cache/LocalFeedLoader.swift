7a2f17e8fd48ddbcde5de6b32e5be51feb488dd1
Rostislav Zapolskiy <rostislav.zapolskiy@gmail.com>
2022-08-29 11:48:56 +0400

    Load command delivers no images on empty cache

--- feed-app/Feed Cache/LocalFeedLoader.swift
+++ feed-app/Feed Cache/LocalFeedLoader.swift
@@ -41,6 +41,8 @@ public class LocalFeedLoader {
         store.retrieve { error in
             if let error = error {
                 completion(.failure(error))
+            } else {
+                completion(.success([]))
             }
         }
     }
