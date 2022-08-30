6ffc4492b113ff724dedc773093b3b33d6c6730c
Rostislav Zapolskiy <rostislav.zapolskiy@gmail.com>
2022-08-30 16:18:07 +0400

    Separated Date extensions

--- feed-appTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
+++ feed-appTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
@@ -20,13 +20,19 @@ func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
 
 extension Date {
     func minusFeedCacheMaxAge() -> Date {
-        return adding(days: -7)
+        return adding(days: -feedCacheMaxAgeInDays)
     }
     
-    func adding(days: Int) -> Date {
+    private var feedCacheMaxAgeInDays: Int {
+        return 7
+    }
+    
+    private func adding(days: Int) -> Date {
         return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
     }
+}
     
+extension Date {
     func adding(seconds: TimeInterval) -> Date {
         return self + seconds
     }
