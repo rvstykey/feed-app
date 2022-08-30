80a220c53e68acd8ae8166733c89bbab6c182fdc
Rostislav Zapolskiy <rostislav.zapolskiy@gmail.com>
2022-08-30 15:22:49 +0400

    Extracted duplicate test helpers into a shared scope

---
+++ feed-appTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
@@ -0,0 +1,29 @@
+//
+//  FeedCacheTesthelpers.swift
+//  feed-appTests
+//
+//  Created by Rostislav Zapolsky on 30.08.22.
+//
+
+import Foundation
+import feed_app
+
+func uniqueImage() -> FeedImage {
+    return FeedImage(id: UUID(), description: "any", location: "any", url: anyURL())
+}
+
+func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
+    let models = [uniqueImage(), uniqueImage()]
+    let local = models.map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
+    return (models, local)
+}
+
+extension Date {
+    func adding(days: Int) -> Date {
+        return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
+    }
+    
+    func adding(seconds: TimeInterval) -> Date {
+        return self + seconds
+    }
+}
---
+++ feed-appTests/Helpers/SharedTestHelpers.swift
@@ -0,0 +1,16 @@
+//
+//  SharedTestHelpers.swift
+//  feed-appTests
+//
+//  Created by Rostislav Zapolsky on 30.08.22.
+//
+
+import Foundation
+
+func anyURL() -> URL {
+    return URL(string: "http://any-url.com")!
+}
+
+func anyNSError() -> NSError {
+    return NSError(domain: "any error", code: 0)
+}
