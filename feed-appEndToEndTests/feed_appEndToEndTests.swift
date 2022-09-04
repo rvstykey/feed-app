8b2050f2bb974e08ed5db1b9971717fb45216d41
Rostislav Zapolskiy <rostislav.zapolskiy@gmail.com>
2022-09-04 22:37:25 +0400

    Updated end-to-end tests with Swift.Result type

--- feed-appEndToEndTests/feed_appEndToEndTests.swift
+++ feed-appEndToEndTests/feed_appEndToEndTests.swift
@@ -27,7 +27,7 @@ class feed_appEndToEndTests: XCTestCase {
     
     // MARK: - Helpers
     
-    private func getFeedResult(file: StaticString = #filePath, line: UInt = #line) -> LoadFeedResult? {
+    private func getFeedResult(file: StaticString = #filePath, line: UInt = #line) -> FeedLoader.Result? {
         let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
         let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
         let loader = RemoteFeedLoader(url: testServerURL, client: client)
@@ -36,7 +36,7 @@ class feed_appEndToEndTests: XCTestCase {
         
         let exp = expectation(description: "Wait for load completion")
         
-        var receivedResult: LoadFeedResult?
+        var receivedResult: FeedLoader.Result?
         loader.load { result in
             receivedResult = result
             exp.fulfill()
