23137c86aca0425913dd700b49acb0c31696d62f
Rostislav Zapolskiy <rostislav.zapolskiy@gmail.com>
2022-09-04 13:01:58 +0400

    LocalFeedLoader in integration with CoreDataFeedStore loads saved images

--- feed_appCacheIntegrationTests/feed_appCacheIntegrationTests.swift
+++ feed_appCacheIntegrationTests/feed_appCacheIntegrationTests.swift
@@ -10,6 +10,18 @@ import feed_app
 
 class feed_appCacheIntegrationTests: XCTestCase {
     
+    override func setUp() {
+         super.setUp()
+
+         setupEmptyStoreState()
+     }
+
+     override func tearDown() {
+         super.tearDown()
+
+         undoStoreSideEffects()
+     }
+    
     func test_load_deliversNoItemsOnEmptyCache() {
         let sut = makeSUT()
         
@@ -28,6 +40,33 @@ class feed_appCacheIntegrationTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
     }
     
+    func test_load_deliversItemsSavedOnASeparateInstance() {
+        let sutToPerformSave = makeSUT()
+        let sutToPerformLoad = makeSUT()
+        let feed = uniqueImageFeed().models
+
+        let saveExp = expectation(description: "Wait for save completion")
+        sutToPerformSave.save(feed) { saveError in
+            XCTAssertNil(saveError, "Expected to save feed successfully")
+            saveExp.fulfill()
+        }
+        wait(for: [saveExp], timeout: 1.0)
+
+        let loadExp = expectation(description: "Wait for load completion")
+        sutToPerformLoad.load { loadResult in
+            switch loadResult {
+            case let .success(imageFeed):
+                XCTAssertEqual(imageFeed, feed)
+
+            case let .failure(error):
+                XCTFail("Expected successful feed result, got \(error) instead")
+            }
+
+            loadExp.fulfill()
+        }
+        wait(for: [loadExp], timeout: 1.0)
+    }
+    
     // MARK: Helpers
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> LocalFeedLoader {
@@ -39,6 +78,18 @@ class feed_appCacheIntegrationTests: XCTestCase {
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
     }
+    
+    private func setupEmptyStoreState() {
+         deleteStoreArtifacts()
+     }
+
+     private func undoStoreSideEffects() {
+         deleteStoreArtifacts()
+     }
+
+     private func deleteStoreArtifacts() {
+         try? FileManager.default.removeItem(at: testSpecificStoreURL())
+     }
     
     private func testSpecificStoreURL() -> URL {
         return cachesDirectory().appendingPathComponent("\(type(of: self)).store")
