6832cfc78fc8bdae90315e56973c9e92228de8cc
Rostislav Zapolskiy <rostislav.zapolskiy@gmail.com>
2022-09-04 10:31:32 +0400

    Add Feed Cache Integration test target

---
+++ feed_appCacheIntegrationTests/feed_appCacheIntegrationTests.swift
@@ -0,0 +1,12 @@
+//
+//  feed_appCacheIntegrationTests.swift
+//  feed_appCacheIntegrationTests
+//
+//  Created by Rostislav Zapolsky on 04.09.22.
+//
+
+import XCTest
+
+class feed_appCacheIntegrationTests: XCTestCase {
+
+}
