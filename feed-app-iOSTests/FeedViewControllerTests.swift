d231a95e7b7ee47bae7ca0c2429d5da81d0c996f
Rostislav Zapolskiy <rostislav.zapolskiy@gmail.com>
2022-09-07 10:44:59 +0400

    Add FeedLoader conformance to LoaderSpy test class

--- feed-app-iOSTests/FeedViewControllerTests.swift
+++ feed-app-iOSTests/FeedViewControllerTests.swift
@@ -7,12 +7,13 @@
 
 import UIKit
 import XCTest
+import feed_app
 
 final class FeedViewController: UIViewController {
     
-    private var loader: FeedViewControllerTests.LoaderSpy?
+    private var loader: FeedLoader?
     
-    convenience init(loader: FeedViewControllerTests.LoaderSpy) {
+    convenience init(loader: FeedLoader) {
         self.init()
         self.loader = loader
     }
@@ -20,7 +21,7 @@ final class FeedViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        loader?.load()
+        loader?.load() { _ in }
     }
 }
 
@@ -44,10 +45,10 @@ class FeedViewControllerTests: XCTestCase {
     
     // MARK: - Helpers
     
-    class LoaderSpy {
+    class LoaderSpy: FeedLoader {
         private(set) var loadCallCount: Int = 0
         
-        func load() {
+        func load(completion: @escaping (FeedLoader.Result) -> Void) {
             loadCallCount += 1
         }
     }
