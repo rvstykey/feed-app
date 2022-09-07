929148098a84bf09a75cb015bfe8163fa66d06bb
Rostislav Zapolskiy <rostislav.zapolskiy@gmail.com>
2022-09-07 10:41:05 +0400

    Loads feed on view did load

--- feed-app-iOSTests/FeedViewControllerTests.swift
+++ feed-app-iOSTests/FeedViewControllerTests.swift
@@ -5,28 +5,51 @@
 //  Created by Rostislav Zapolsky on 07.09.22.
 //
 
-import Foundation
+import UIKit
 import XCTest
 
-final class FeedViewController {
-     init(loader: FeedViewControllerTests.LoaderSpy) {
-
-     }
- }
+final class FeedViewController: UIViewController {
+    
+    private var loader: FeedViewControllerTests.LoaderSpy?
+    
+    convenience init(loader: FeedViewControllerTests.LoaderSpy) {
+        self.init()
+        self.loader = loader
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loader?.load()
+    }
+}
 
 class FeedViewControllerTests: XCTestCase {
     
     func test_init_doesNotLoadFeed() {
-         let loader = LoaderSpy()
-         _ = FeedViewController(loader: loader)
-
-         XCTAssertEqual(loader.loadCallCount, 0)
-     }
+        let loader = LoaderSpy()
+        _ = FeedViewController(loader: loader)
+        
+        XCTAssertEqual(loader.loadCallCount, 0)
+    }
+    
+    func test_viewDidLoad_loadsFeed() {
+        let loader = LoaderSpy()
+        let sut = FeedViewController(loader: loader)
+        
+        sut.loadViewIfNeeded()
+        
+        XCTAssertEqual(loader.loadCallCount, 1)
+    }
     
     // MARK: - Helpers
-     
-     class LoaderSpy {
-         private(set) var loadCallCount: Int = 0
-     }
+    
+    class LoaderSpy {
+        private(set) var loadCallCount: Int = 0
+        
+        func load() {
+            loadCallCount += 1
+        }
+    }
     
 }
