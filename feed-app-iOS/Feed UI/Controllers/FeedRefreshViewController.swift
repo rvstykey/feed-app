f756f6c8e9c7817447b6f8a7e146dce6a0ed6db0
Rostislav Zapolskiy <rostislav.zapolskiy@gmail.com>
2022-11-13 10:41:36 +0400

    Extract UIRefreshControl management to separate FeedRefreshViewController

---
+++ feed-app-iOS/Feed UI/Controllers/FeedRefreshViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FeedRefreshViewController.swift
+//  feed-app-iOS
+//
+//  Created by Rostislav Zapolsky on 13.11.22.
+//
+
+import UIKit
+import feed_app
+
+final class FeedRefreshViewController: NSObject {
+    private(set) lazy var view: UIRefreshControl = {
+        let view = UIRefreshControl()
+        view.addTarget(self, action: #selector(refresh), for: .valueChanged)
+        return view
+    }()
+    
+    let feedLoader: FeedLoader
+    
+    init(feedLoader: FeedLoader) {
+        self.feedLoader = feedLoader
+    }
+    
+    var onRefresh: (([FeedImage]) -> Void)?
+    
+    @objc func refresh() {
+        view.beginRefreshing()
+        feedLoader.load { [weak self] result in
+            if let feed = try? result.get() {
+                self?.onRefresh?(feed)
+            }
+            self?.view.endRefreshing()
+        }
+    }
+}
