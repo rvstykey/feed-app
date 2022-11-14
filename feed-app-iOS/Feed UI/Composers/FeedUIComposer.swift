461c5afa770910a34fc18e5d46799f48375eafff
Rostislav Zapolskiy <rostislav.zapolskiy@gmail.com>
2022-11-14 12:52:09 +0400

    Extracted adaption FeedViewController to FeedImage into separate method

--- feed-app-iOS/Feed UI/Composers/FeedUIComposer.swift
+++ feed-app-iOS/Feed UI/Composers/FeedUIComposer.swift
@@ -13,11 +13,17 @@ public final class FeedUIComposer {
     public static func feedComposedWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
         let refreshController = FeedRefreshViewController(feedLoader: feedLoader)
         let feedController = FeedViewController(refreshController: refreshController)
-        refreshController.onRefresh = { [weak feedController] feed in
-            feedController?.tableModel = feed.map {
-                return FeedImageCellController(model: $0, imageLoader: imageLoader)
+        refreshController.onRefresh = adaptFeedToCellControllers(forwardingTo: feedController, loader: imageLoader)
+        return feedController
+    }
+}
+
+private extension FeedUIComposer {
+    private static func adaptFeedToCellControllers(forwardingTo controller: FeedViewController, loader: FeedImageDataLoader) -> ([FeedImage]) -> Void {
+        return { [weak controller] feed in
+            controller?.tableModel = feed.map {
+                return FeedImageCellController(model: $0, imageLoader: loader)
             }
         }
-        return feedController
     }
 }
