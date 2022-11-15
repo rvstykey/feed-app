b36bea6f257991f2ba66750a433d4365507e7263
Rostislav Zapolskiy <rostislav.zapolskiy@gmail.com>
2022-11-15 11:58:00 +0400

    Removed mutable state from FeedViewModel

--- feed-app-iOS/Feed UI/Models/FeedViewModel.swift
+++ feed-app-iOS/Feed UI/Models/FeedViewModel.swift
@@ -9,26 +9,24 @@ import Foundation
 import feed_app
 
 final class FeedViewModel {
-    let feedLoader: FeedLoader
+    typealias Observer<T> = (T) -> Void
+    
+    private let feedLoader: FeedLoader
     
     init(feedLoader: FeedLoader) {
         self.feedLoader = feedLoader
     }
     
-    var onChange: ((FeedViewModel) -> Void)?
-    var onFeedLoad: (([FeedImage]) -> Void)?
-    
-    var isLoading: Bool = false {
-        didSet { onChange?(self) }
-    }
+    var onLoadingStateChange: Observer<Bool>?
+    var onFeedLoad: Observer<[FeedImage]>?
     
     func loadFeed() {
-        isLoading = true
+        onLoadingStateChange?(true)
         feedLoader.load { [weak self] result in
             if let feed = try? result.get() {
                 self?.onFeedLoad?(feed)
             }
-            self?.isLoading = false
+            self?.onLoadingStateChange?(false)
         }
     }
 }
