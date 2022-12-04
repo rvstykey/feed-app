e4c72a51321cbadc136e5c86be0b3bfc7d4ebe09
Rostislav Zapolskiy <rostislav.zapolskiy@gmail.com>
2022-12-04 14:24:37 +0400

    Duplicate RemoteFeedLoader as RemoteImageCommentsLoader

---
+++ feed-app/feed-app/Feed API/ImageCommentsMapper.swift
@@ -0,0 +1,22 @@
+//
+//  ImageCommentsMapper.swift
+//  feed-app
+//
+//  Created by Rostislav Zapolsky on 04.12.22.
+//
+
+import Foundation
+
+final class ImageCommentsMapper {
+    private struct Root: Decodable {
+        let items: [RemoteFeedItem]
+    }
+    
+    static func map(_ data: Data, response: HTTPURLResponse) throws -> [RemoteFeedItem] {
+        guard response.isOK, let root = try? JSONDecoder().decode(Root.self, from: data) else {
+            throw RemoteImageCommentsLoader.Error.invalidData
+        }
+        
+        return root.items
+    }
+}
