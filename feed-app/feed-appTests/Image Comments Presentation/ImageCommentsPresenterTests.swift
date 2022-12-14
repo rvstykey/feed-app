8c399a603d12d4d779c258b9372f492d77735813
Rostislav Zapolskiy <rostislav.zapolskiy@gmail.com>
2022-12-14 16:50:41 +0400

    Move UIImage creation to a tryMake extension

---
+++ feed-app/feed-appTests/Image Comments Presentation/ImageCommentsPresenterTests.swift
@@ -0,0 +1,12 @@
+//
+//  ImageCommentsPresenterTests.swift
+//  feed-appTests
+//
+//  Created by Rostislav Zapolsky on 14.12.22.
+//
+
+import XCTest
+
+final class ImageCommentsPresenterTests: XCTestCase {
+    
+}
