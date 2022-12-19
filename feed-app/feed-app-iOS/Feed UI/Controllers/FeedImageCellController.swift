d6c858e5e329cdb5358d5989494306d2486019b7
Rostislav Zapolskiy <rostislav.zapolskiy@gmail.com>
2022-12-19 10:05:43 +0400

    Revert "Revert "Capture cell reference and load/reload cell resources on `willDisplayCell` as a fix for iOS 15 cell prefetching behavior change.""
    This reverts commit 889a6bca91865b79ecb530466c544bed3c83a559.

--- feed-app/feed-app-iOS/Feed UI/Controllers/FeedImageCellController.swift
+++ feed-app/feed-app-iOS/Feed UI/Controllers/FeedImageCellController.swift
@@ -51,6 +51,11 @@ extension FeedImageCellController: UITableViewDataSource, UITableViewDelegate, U
         delegate.didRequestImage()
     }
     
+    public func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        self.cell = cell as? FeedImageCell
+        delegate.didRequestImage()
+    }
+    
     public func tableView(_ tableView: UITableView, cancelPrefetchingForRowsAt indexPaths: [IndexPath]) {
         cancelLoad()
     }
