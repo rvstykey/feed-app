575c74df3b0d9acae4ce77f15e1dd3af20522769
Rostislav Zapolskiy <rostislav.zapolskiy@gmail.com>
2022-12-19 08:46:11 +0400

    Run ListSnapshotTests without storyboard

--- feed-app/feed-app-iOSTests/Shared UI/ListSnapshotTests.swift
+++ feed-app/feed-app-iOSTests/Shared UI/ListSnapshotTests.swift
@@ -20,23 +20,21 @@ final class ListSnapshotTests: XCTestCase {
         assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "EMPTY_LIST_dark")
     }
     
-    // TODO: Github Actions produce different version of snapshot. Disabled for now.
-//    func test_feedWithErrorMessage() {
-//        let sut = makeSUT()
-//
-//        sut.display(.error(message: "This is a\nmulti-line\nerror message"))
-//
-//        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "LIST_WITH_ERROR_MESSAGE_light")
-//        assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "LIST_WITH_ERROR_MESSAGE_dark")
-//    }
+    func test_feedWithErrorMessage() {
+        let sut = makeSUT()
+
+        sut.display(.error(message: "This is a\nmulti-line\nerror message"))
+
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "LIST_WITH_ERROR_MESSAGE_light")
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "LIST_WITH_ERROR_MESSAGE_dark")
+    }
     
     // MARK: - Helpers
     
     private func makeSUT() -> ListViewController {
-        let bundle = Bundle(for: ListViewController.self)
-        let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
-        let controller = storyboard.instantiateInitialViewController() as! ListViewController
+        let controller = ListViewController()
         controller.loadViewIfNeeded()
+        controller.tableView.separatorStyle = .none
         controller.tableView.showsVerticalScrollIndicator = false
         controller.tableView.showsHorizontalScrollIndicator = false
         return controller
