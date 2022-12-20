b1295bc5fc7a20e59ea5aaf2aef92c62e9fdb93a
Rostislav Zapolskiy <rostislav.zapolskiy@gmail.com>
2022-12-20 10:25:26 +0400

    Duplicate FeedUIComposer as CommentsUIComposer

---
+++ main-app/main-app/CommentsUIComposer.swift
@@ -0,0 +1,42 @@
+//
+//  CommentsUIComposer.swift
+//  main-app
+//
+//  Created by Rostislav Zapolsky on 20.12.22.
+//
+
+import UIKit
+import Combine
+import feed_app
+import feed_app_iOS
+
+public final class CommentsUIComposer {
+    private init() {}
+    
+    private typealias FeedPresentationAdapter = LoadResourcePresentationAdapter<[FeedImage], FeedViewAdapter>
+    
+    public static func commentsComposedWith(
+        commentsLoader: @escaping () -> AnyPublisher<[FeedImage], Error>
+    ) -> ListViewController {
+        let presentationAdapter = FeedPresentationAdapter(loader: commentsLoader)
+        let feedController = makeFeedViewController(title: FeedPresenter.title)
+        feedController.onRefresh = presentationAdapter.loadResource
+        
+        presentationAdapter.presenter = LoadResourcePresenter(
+            resourceView: FeedViewAdapter(
+                controller: feedController,
+                imageLoader: { _ in Empty<Data, Error>().eraseToAnyPublisher()}),
+            loadingView: WeakRefVirtualProxy(feedController),
+            errorView: WeakRefVirtualProxy(feedController),
+            mapper: FeedPresenter.map)
+        return feedController
+    }
+    
+     static func makeFeedViewController(title: String) -> ListViewController {
+         let bundle = Bundle(for: ListViewController.self)
+         let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
+         let feedController = storyboard.instantiateInitialViewController() as! ListViewController
+         feedController.title = title
+         return feedController
+     }
+ }
