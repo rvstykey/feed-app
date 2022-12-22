75197ccbc37a9dee3f23c10e2ec5a26fc6e7e1ad
Rostislav Zapolskiy <rostislav.zapolskiy@gmail.com>
2022-12-22 10:39:50 +0400

    Show comments on image selection

--- main-app/main-appTests/FeedAcceptanceTests.swift
+++ main-app/main-appTests/FeedAcceptanceTests.swift
@@ -53,7 +53,14 @@ class FeedAcceptanceTests: XCTestCase {
         enterBackground(with: store)
         
         XCTAssertNotNil(store.feedCache, "Expected to keep non-expired cache")
-     }
+    }
+    
+    func test_onFeedImageSelection_displaysComments() {
+        let comments = showCommentsForFirstImage()
+        
+        XCTAssertEqual(comments.numberOfRenderedComments(), 1)
+        XCTAssertEqual(comments.commentMessage(at: 0), makeCommentMessage())
+    }
     
     // MARK: - Helpers
     
@@ -74,6 +81,16 @@ class FeedAcceptanceTests: XCTestCase {
         sut.sceneWillResignActive(UIApplication.shared.connectedScenes.first!)
     }
     
+    private func showCommentsForFirstImage() -> ListViewController {
+        let feed = launch(httpClient: .online(response), store: .empty)
+        
+        feed.simulateTapOnFeedImage(at: 0)
+        RunLoop.current.run(until: Date())
+        
+        let nav = feed.navigationController
+        return nav?.topViewController as! ListViewController
+    }
+    
     private func response(for url: URL) -> (Data, HTTPURLResponse) {
         let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)!
         return (makeData(for: url), response)
@@ -86,6 +103,9 @@ class FeedAcceptanceTests: XCTestCase {
             
         case "/essential-feed/v1/feed":
             return makeFeedData()
+            
+        case "/essential-feed/v1/image/2AB2AE66-A4B7-4A16-B374-51BBAC8DB086/comments":
+            return makeCommentsData()
             
         default:
             return Data()
@@ -103,4 +123,20 @@ class FeedAcceptanceTests: XCTestCase {
         ]])
     }
     
+    private func makeCommentsData() -> Data {
+        return try! JSONSerialization.data(withJSONObject: ["items": [
+            [
+                "id": UUID().uuidString,
+                "message": makeCommentMessage(),
+                "created_at": "2020-05-20T11:24:59+0000",
+                "author": [
+                    "username": "a username"
+                ]
+            ]
+        ]])
+    }
+    
+    private func makeCommentMessage() -> String {
+        "a message"
+    }
 }
