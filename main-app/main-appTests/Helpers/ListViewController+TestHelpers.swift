68bb3c88b087c884eb1da2f3379fa99a8bcda4b2
Rostislav Zapolskiy <rostislav.zapolskiy@gmail.com>
2022-12-30 17:16:12 +0400

    Render load more error message

--- main-app/main-appTests/Helpers/ListViewController+TestHelpers.swift
+++ main-app/main-appTests/Helpers/ListViewController+TestHelpers.swift
@@ -129,6 +129,10 @@ extension ListViewController {
         return loadMoreFeedCell()?.isLoading == true
     }
     
+    var loadMoreFeedErrorMessage: String? {
+        return loadMoreFeedCell()?.message
+    }
+    
     private func loadMoreFeedCell() -> LoadMoreCell? {
         cell(row: 0, section: feedLoadMoreSection) as? LoadMoreCell
     }
